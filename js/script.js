b690676d28886926d77535f3c84c319d9bfbc2e7
michael <mikelkarije@gmail.com>
2019-07-15 11:46:16 +0300

    add readMe.md

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var dayOfweek = function(cc, yy, mm, dd){
     return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
 };
-var months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "november", "december"];
+if (month 0>31)
+alert(month)
