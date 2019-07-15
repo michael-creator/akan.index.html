9f9f2b7f184a788cf8b71783498d7072c92f3bee
michael <mikelkarije@gmail.com>
2019-07-15 13:00:26 +0300

    add new variables to the page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 var dayOfweek = function(cc, yy, mm, dd){
     return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
 };
-if (month 0>31)
-alert(month)
+if (mm=<0)||(mm>31){
+    alert(invalid)
+}
+if (yy=<0)||(yy>12)
+{
+    alert(invalid)
+}
