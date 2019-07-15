cb6a3fa976c4a0073ed94c776cdbb66bb0eeb212
michael <mikelkarije@gmail.com>
2019-07-15 08:24:46 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 var dayOfweek = function(cc, yy, mm, dd){
     return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
 };
+var months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "november", "december"];
